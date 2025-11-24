0c75cb800d0ee0f738768d8936ded6504fff8e34
jiutian303jeff <youxizhu80@gmail.com>
2025-11-24 15:28:43 -0500

    hello

--- mian.py
+++ mian.py
@@ -5,10 +5,14 @@ import time
 class Gui:
     def __init__(self):
         self.main_win = tk.Tk()
-        self.frame1 = tk.Frame(self.main_win).pack()
-        self.frame2 = tk.Frame(self.main_win).pack()
-        self.frame3 = tk.Frame(self.main_win).pack()
-        self.frame4 = tk.Frame(self.main_win).pack()
+        self.frame1 = tk.Frame(self.main_win)
+        self.frame1.pack()
+        self.frame2 = tk.Frame(self.main_win)
+        self.frame2.pack()
+        self.frame3 = tk.Frame(self.main_win)
+        self.frame3.pack()
+        self.frame4 = tk.Frame(self.main_win)
+        self.frame4.pack()
 
 
         self.label_welcome = tk.Label(self.frame1, text="Welcome", font=("Arial",20))
@@ -27,6 +31,20 @@ class Gui:
 
         self.enter_password = tk.Entry(self.frame3, width=20)
         self.enter_password.pack(side="right")
+
+        #calling another function and page for login
+        self.login = tk.Button(self.frame4, text="Login", command=self.logging)
+        self.login.pack()
+        
+        #calling another function and page for creating new account
+        self.create_account = tk.Button(self.frame4, text="Create an account", command=self.create)
+
+
+
+    def logging(self):
+        pass
+    def create(self):
+        pass
 
 
         self.main_win.mainloop()
