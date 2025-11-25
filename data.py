87651993ca3c5ec559dff04adc421554dd6b8721
omghk94877 <omghk94877@gmail.com>
2025-11-25 10:55:09 -0500

    smt new

---
+++ data.py
@@ -0,0 +1,22 @@
+"""
+Docstring for data
+"""
+
+class Data :
+    def __init__(self):
+        self.username = ""
+        self.password = ""
+        self.balance = 0
+        self.transaction_history = []
+
+    def save_data(self):
+        pass
+
+    def balance(self):
+        pass
+
+    def trans_history(self):
+        pass
+
+    def error_handle(self):
+        pass
